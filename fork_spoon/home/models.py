1735e9e2a1def12c83492b7fcd71c5d6610677f2
Zane-dev16 <irellzanedev@gmail.com>
2021-02-24 14:40:34 +0000

    Update models and add static files

--- fork_spoon/home/models.py
+++ fork_spoon/home/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class Hero(models.Model):
-    title = models.CharField(max_length = 100)
     button_text = models.CharField(max_length=30, default="")
     slide = models.ImageField(upload_to='images/', default = "")
     # date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.title
+        return self.button_text
 
     class Meta:
         verbose_name = "Hero Slide"
